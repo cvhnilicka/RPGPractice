14c9349392224593832c04f4d6d65c3d8293aeb3
cvhnilicka <cvhnilicka@wisc.edu>
2020-08-18 11:21:18 -0700

    first commit for rpg like game for practice

---
+++ Assets/Mover.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Experimental.Rendering;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            MoveToCursor();
+        }
+        
+        
+    }
+
+    private void MoveToCursor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        bool hasHit = Physics.Raycast(ray, out hitInfo);
+        if (hasHit)
+        {
+            GetComponent<NavMeshAgent>().destination = hitInfo.point;
+        }
+        
+    }
+}
