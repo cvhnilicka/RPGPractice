3bb4135aa5ece1c51512a3fd5f81fad16b766948
cvhnilicka <cvhnilicka@wisc.edu>
2020-08-19 09:34:42 -0700

    restructure

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using RPG.Movement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            MouseInputHandler();
+        }
+
+
+        private void MouseInputHandler()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+
+
+
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            bool hasHit = Physics.Raycast(ray, out hitInfo);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hitInfo.point);
+            }
+
+        }
+    }
+}
+
