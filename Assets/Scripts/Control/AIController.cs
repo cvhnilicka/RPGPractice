cb5f4c424a924ef87d53b90d94e648ad2c2de972
cvhnilicka <cvhnilicka@wisc.edu>
2020-08-26 13:02:09 -0700

    beginnings of ai controller' :

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f; // default 5 meters
+
+
+        private void Update()
+        {
+            
+            if (GetInRange())
+            {
+                print("Enemy : " + this.name + " should give chase");
+            }
+
+        }
+
+        private bool GetInRange()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(player.transform.position, transform.position) < chaseDistance;
+        }
+
+    }
+
+}
