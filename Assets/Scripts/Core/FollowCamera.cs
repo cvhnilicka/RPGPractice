49b82c357d2e01e84254747ae7bb8ff124d3e90d
cvhnilicka <cvhnilicka@wisc.edu>
2020-08-31 10:11:38 -0700

    starting to mess with a follow camera rotating as well

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -9,10 +9,23 @@ namespace RPG.Core
     {
         [SerializeField] Transform target;
 
+        [SerializeField] float smoothSpeed = 0.5f;
+
+        Vector3 offset;
+
+        private void Start()
+        {
+            offset = target.position - transform.position;
+        }
+
         // Update is called once per frame
         void LateUpdate()
         {
-            transform.position = target.position;
+
+
+            Vector3 smoothPosition = Vector3.Lerp(transform.position, target.position, smoothSpeed*Time.deltaTime);
+            transform.position = smoothPosition;
+            transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, Time.deltaTime * smoothSpeed);
         }
     }
 }
